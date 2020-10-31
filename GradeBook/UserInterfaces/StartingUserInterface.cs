ef6948497ce378bbb9859cd0f42e7051fdf67bec
Dawid Dziad <dawid.dziad@microsoft.wsei.edu.pl>
2020-10-31 15:14:27 +0100

    1. Dodanie bool'a "IsWeighted "do konstruktorów: BaseGradeBook, StandardGradeBook oraz RankedGradeBook. 2. Aktualizacja warunku oraz wiadomości dla użytkownika w "CreateCommand".

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -34,19 +34,23 @@ namespace GradeBook.UserInterfaces
         public static void CreateCommand(string command)
         {
             var parts = command.Split(' ');
-            if (parts.Length != 3)
+            if (parts.Length != 4)
             {
-                Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
+                Console.WriteLine("Command not valid, Create requires a name, type of gradebook, if it's weighted (true / false).");
                 return;
             }
             var name = parts[1];
             var typeGradebook = parts[2];
 
+            var weighted = false;
+            if (parts[3] == "true")
+                weighted = true;
+
             BaseGradeBook gradeBook;
             if (typeGradebook == "standard")
-                gradeBook = new StandardGradeBook(name);
+                gradeBook = new StandardGradeBook(name, weighted);
             if (typeGradebook == "ranked")
-                gradeBook = new RankedGradeBook(name);
+                gradeBook = new RankedGradeBook(name, weighted);
             else
             {
                 Console.WriteLine("{0} is not a supported type of gradebook, please try again", typeGradebook);
